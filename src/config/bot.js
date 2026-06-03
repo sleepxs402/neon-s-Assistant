cc32a6884aeed02d3d7e071ca83abc91781d9b97
sleepxs402 <sleepxs402@gmail.com>
2026-06-03 22:12:53 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made by neon",
+        name: "👑 Made by neon 🚀",
         // Activity type number (0 = Playing).
         type: 1, 
       },
